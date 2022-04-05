cf8b1e70b13472b95d0fa8239ed0fbe5cd488fae
Alessio1921 <caruso.alessio1999@gmail.com>
2022-04-05 18:19:53 +0200

    first commit

---
+++ esercizio1.py
@@ -0,0 +1,4 @@
+firstAddend = input("inserisci il primo addendo ");
+secondAddend = input("inserisci il secondo addendo ");
+total=int(firstAddend) * int(secondAddend)
+print("il risultato è" +" "+ str(total))
