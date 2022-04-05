e2494dabc7c129c21f1e03720c84d76fca45ac45
Alessio1921 <caruso.alessio1999@gmail.com>
2022-04-05 18:25:48 +0200

    modified int with float

--- esercizio1.py
+++ esercizio1.py
@@ -1,4 +1,4 @@
 firstAddend = input("inserisci il primo addendo ");
 secondAddend = input("inserisci il secondo addendo ");
-total=int(firstAddend) * int(secondAddend)
+total=float(firstAddend) * float(secondAddend)
 print("il risultato è" + " " + str(total))
